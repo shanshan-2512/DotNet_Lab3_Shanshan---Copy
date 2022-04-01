d1f9735919aacdaafcb601f62d4378d941fb7c5f
Shanshan <1896768@cegep-heritage.qc.ca>
2022-04-01 13:14:34 -0400

    login commit

--- Pages/Product/Index.cshtml.cs
+++ Pages/Product/Index.cshtml.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using DotNet_Lab3_Shanshan.Models;
+using System.Security.Claims;
 
 namespace _Net_Lab3_Shanshan.Pages_Product
 {
@@ -19,11 +20,38 @@ namespace _Net_Lab3_Shanshan.Pages_Product
             _context = context;
         }
 
-        public IList<Product> Product { get;set; }
+        public IList<Product> Product { get; set; }
 
+        public string? UserEmail { get; set; }
         public async Task OnGetAsync()
         {
+
             Product = await _context.Product.ToListAsync();
+
+            if (User.Identity != null)
+
+            {
+
+                var claimsIdentity = (ClaimsIdentity)User.Identity;
+
+                if (claimsIdentity.IsAuthenticated)
+
+                {
+
+                    var email = claimsIdentity.FindFirst(ClaimTypes.Email);
+
+                    if (email != null)
+
+                    {
+                        UserEmail = email.Value;
+                    }
+
+
+
+                }
+            }
         }
     }
 }
+
+
